224b4ddd60a68a70f78564c9f1a0b2a54e7e13e1
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-01-16 16:39:58 +0600

    all big o explained

--- lesson01-big0.js
+++ lesson01-big0.js
@@ -33,3 +33,54 @@ const searchForItem = (item) => {
 
 searchForItem("eggs");
 
+//O(1) -constant == const keyword and not going to be change
+
+const numberss = [1, 2, 3, 4, 5];
+const getElement = (arr, index) => arr[index];
+console.log(getElement(numberss, 0));
+
+
+// O(n^2)
+
+// ------------------------------------
+// function findPairs(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       console.log(`Pair: ${arr[i]}, ${arr[j]}`);
+//     }
+//   }
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// findPairs(numbers);
+// ------------------------------------
+
+// ------------------------------------
+// Drop Non-Dominants
+function findPairs(arr) {
+  // O(n^2)
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(`Pair: ${arr[i]}, ${arr[j]}`);
+    }
+  }
+
+  // O(n)
+  for (let q = 0; q < 5; q++) {
+    console.log("-------------", q);
+  }
+
+  // If we combine all the "O" operations it becomes O(n^2 + n)
+  // O(n^2) is a Dominant term
+  // "n" is a Non-Dominant term
+  // So we remove the "non-dominant" term and we're only left with O(n^2)
+  // This way, we simplify our bigO
+}
+
+const numbers = [1, 2, 3, 4, 5];
+findPairs(numbers);
+// ------------------------------------
+
+
+
+//O(log(n))
